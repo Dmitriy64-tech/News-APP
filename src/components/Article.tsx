28eee65e9211e4b16d218bd4c4bdd578e546d6e4
Dmitriy64-tech <danilin.dmitriy4769@gmail.com>
2021-09-02 12:17:42 +0300

    Style fixes

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -8,12 +8,12 @@ type PropsType ={
 export const Article = (props: PropsType) => {
     return (
         <div className="Article">
-            <h2>{props.article.title}</h2>
+            <h2 className="Title">{props.article.title}</h2>
             <img alt="" className="ArticleImg" src={props.article.urlToImage}></img>
-            <p className="Content">{props.article.content}</p>
-            <div>Sourse: {props.article.source.name}</div>
-            <div>Author: {props.article.author}</div>
-            <a href={props.article.url}>Link</a>
+            <p className="Content">     {props.article.content}</p>
+            <div className="Source">Sourse: {props.article.source.name}</div>
+            <div className="Author">Author: {props.article.author}</div>
+            <a className="Link" href={props.article.url}>Link</a>
         </div>
     )
 }
